89c6fbf02bc59611fa1a023f9200d51cf0883fb7
Marc Jersey Castro <marcjerseycastro@gmail.com>
2024-11-11 19:13:17 +0800

    Added functions
    -GetPlayerChoice
    -GenerateComputerChoice
    -Compare
    
    Then print the output

--- index.js
+++ index.js
@@ -1,15 +1,69 @@
 
-//   initializeGame();
 
+
+// GLOBAL VARIABLES
+let playerChoice;
+let computerChoice;
+
+
+function GetPlayerChoice()
+{
+    const choicesArray = document.querySelectorAll('.choice');
+
+    // Add event listener to each choice
+    choicesArray.forEach(choice =>
+    {
+        choice.addEventListener('click', function ()
+        {
+            // Get the player's choice
+            playerChoice = choice.classList[1]; // clasList[1] returns the second class name
+
+            console.log("You chose: " + playerChoice);
+
+
+            // Generate Computer Choice when the player has chosen
+            GenerateComputerChoice();
+        })
+
+    })
+}
+GetPlayerChoice();
+
+function GenerateComputerChoice()
+{
+    let choices = ["rock", "paper", "scissor"];
+
+    const randomNumber = Math.floor(Math.random() * choices.length);
+
+    computerChoice = choices[randomNumber];
+
+
+    // Compare after the computer has chosen
+    CompareChoices();
+}
+
+
+function CompareChoices()
+{
+    // Compare playerChoice and computerChoice
+    let result = playerChoice === computerChoice ? "Tie game" :
+        playerChoice === "rock" && computerChoice === "paper" ? "Computer Wins" :
+            playerChoice === "paper" && computerChoice === "scissor" ? "Computer Wins" :
+                playerChoice === "scissor" && computerChoice === "rock" ? "Computer Wins" :
+                    "Player Wins";
+
+    console.log(("Player Chose:           " + playerChoice +
+        "\nComputer Chose:    " + computerChoice +
+        "\nResults:                    " + result));
+}
+
+/* 
 function initializeGame()
 {
     confirm("Shall we play rock, paper, scissors?") ? GetPlayerChoice() : alert("Ok, Maybe Next Time!");
 }
 
 
-// GLOBAL VARIABLES
-let playerChoice;
-let computerChoice;
 
 function GetPlayerChoice()
 {
@@ -71,5 +125,5 @@ function PlayAgain()
     {
         alert("Ok, Let's play next time!  :D");
     }
-}
-initializeGame();
+} */
+/* initializeGame(); */
